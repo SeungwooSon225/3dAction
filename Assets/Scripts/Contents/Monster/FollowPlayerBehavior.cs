6ec8bbf1accb689341a129c7bb7d5f02d9536089
Seungwoo Son <sonseungw00123@gamil.com>
2025-02-06 20:45:14 +0900

    add projectile explosion

--- Assets/Scripts/Contents/Monster/FollowPlayerBehavior.cs
+++ Assets/Scripts/Contents/Monster/FollowPlayerBehavior.cs
@@ -46,15 +46,15 @@ public class FollowPlayerBehavior : IBehavior
 
             _elapsedTime += Time.deltaTime;
 
-            //if (_elapsedTime > _monsterStat.EscapeThreshold)
-            //{
-            //    _elapsedTime = 0f;
+            if (_elapsedTime > _monsterStat.EscapeThreshold)
+            {
+                _elapsedTime = 0f;
 
-            //    if (_animator.GetBool("Fly Forward"))
-            //        _animator.SetBool("Fly Forward", false);
+                if (_animator.GetBool("Fly Forward"))
+                    _animator.SetBool("Fly Forward", false);
 
-            //    return BehaviorState.Success;
-            //}
+                return BehaviorState.Success;
+            }
 
             return BehaviorState.Failure;
         }
@@ -73,60 +73,75 @@ public class FollowPlayerBehavior : IBehavior
     {
         _pathFindingTimer += Time.deltaTime;
 
-        // 0.25초마다 실행
+        // 0.1초마다 실행
         if (_pathFindingTimer > 0.1f)
         {
             //Debug.Log("PathFinding");
 
             _pathFindingTimer = 0f;
 
-            // 몬스터와 플레이어 사이에 장애물이 있는지 검사: Y -> AStarPathFinding 실행, N -> 직선 거리로 이동
-            Vector3 direction = _player.position - _monster.position;
-            float distance = direction.magnitude; 
-            if (Physics.Raycast(_monster.position + Vector3.up * 0.5f, direction.normalized, out RaycastHit hit, distance))
+            Node node = Managers.AStar.FindPath(_monster.gameObject, _player.gameObject);
+            if (node == null)
+                return false;
+
+            // 부술 수 있는 장애물
+            if (node.NodeType == NodeType.RemovableObstacle)
             {
-                Node node = Managers.AStar.FindPath(_monster.gameObject, _player.gameObject);
-
-                // 장애물 있을 때
-                if (hit.collider.CompareTag("Obstacle") || hit.collider.CompareTag("RemovableObstacle"))
-                {
-                    //Debug.Log($"Obstacle 발견");
-                    //Node node = Managers.AStar.FindPath(_monster.gameObject, _player.gameObject);
-
-                    if (node == null)
-                        return false;
-
-                    //if (hit.collider.CompareTag("RemovableObstacle"))
-                    //    Debug.Log(Managers.AStar.CurrentCost + "  " + distance);
-
-                    // 부술 수 있는 장애물
-                    //if (hit.collider.CompareTag("RemovableObstacle") && (distance + 10f < Managers.AStar.CurrentCost))
-                    if (hit.collider.CompareTag("RemovableObstacle") && node.NodeType == NodeType.RemovableObstacle)
-                    {
-                        Debug.Log(Managers.AStar.CurrentCost + "  " + distance);
-
-                        _monsterStat.Target = hit.transform;
-                        _moveDestination = hit.transform.position;                     
-                    }
-                    else
-                    {
-                        _monsterStat.Target = _player;
-                        _moveDestination = new Vector3(node.Position.x, 0, node.Position.y);
-                    }
-
-                }
-                // 장애물 없을 때
-                else
-                {
-                    _monsterStat.Target = _player;
-                    //_moveDestination = _player.position;
-                    _moveDestination = new Vector3(node.Position.x, 0, node.Position.y);
-                }
+                _monsterStat.Target = node.Object.transform;
+                _moveDestination = node.Object.transform.position;
             }
-        }
+            else
+            {
+                _monsterStat.Target = _player;
+                _moveDestination = new Vector3(node.Position.x, 0, node.Position.y);
+            }
 
-        //_monster.rotation = Quaternion.Slerp(_monster.rotation, Quaternion.LookRotation(new Vector3(_moveDestination.x, 0, _moveDestination.z) - _monster.position), 10f * Time.deltaTime);
-        //_monster.position += _monster.forward * Time.deltaTime * _monsterStat.MoveSpeed;
+            #region legacy
+            //// 몬스터와 플레이어 사이에 장애물이 있는지 검사: Y -> AStarPathFinding 실행, N -> 직선 거리로 이동
+            //Vector3 direction = _player.position - _monster.position;
+            //float distance = direction.magnitude; 
+            //if (Physics.Raycast(_monster.position + Vector3.up * 0.5f, direction.normalized, out RaycastHit hit, distance))
+            //{
+            //    Node node = Managers.AStar.FindPath(_monster.gameObject, _player.gameObject);
+
+            //    // 장애물 있을 때
+            //    if (hit.collider.CompareTag("Obstacle") || hit.collider.CompareTag("RemovableObstacle"))
+            //    {
+            //        //Debug.Log($"Obstacle 발견");
+            //        //Node node = Managers.AStar.FindPath(_monster.gameObject, _player.gameObject);
+
+            //        if (node == null)
+            //            return false;
+
+            //        //if (hit.collider.CompareTag("RemovableObstacle"))
+            //        //    Debug.Log(Managers.AStar.CurrentCost + "  " + distance);
+
+            //        // 부술 수 있는 장애물
+            //        //if (hit.collider.CompareTag("RemovableObstacle") && (distance + 10f < Managers.AStar.CurrentCost))
+            //        if (hit.collider.CompareTag("RemovableObstacle") && node.NodeType == NodeType.RemovableObstacle)
+            //        {
+            //            Debug.Log(Managers.AStar.CurrentCost + "  " + distance);
+
+            //            _monsterStat.Target = hit.transform;
+            //            _moveDestination = hit.transform.position;                     
+            //        }
+            //        else
+            //        {
+            //            _monsterStat.Target = _player;
+            //            _moveDestination = new Vector3(node.Position.x, 0, node.Position.y);
+            //        }
+
+            //    }
+            //    // 장애물 없을 때
+            //    else
+            //    {
+            //        _monsterStat.Target = _player;
+            //        //_moveDestination = _player.position;
+            //        _moveDestination = new Vector3(node.Position.x, 0, node.Position.y);
+            //    }
+            //}
+            #endregion
+        }
 
         _monster.rotation = Quaternion.Slerp(_monster.rotation, Quaternion.LookRotation(_monsterStat.Target.position - _monster.position), 10f * Time.deltaTime);
         _monster.position += (_moveDestination - _monster.position) * Time.deltaTime * _monsterStat.MoveSpeed;
